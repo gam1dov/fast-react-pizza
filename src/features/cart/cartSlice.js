886407f2b768c07bc6cb9b0b5b18dc634f8fbe1c
Emil Gamidov <dayde1991@mail.ru>
2024-09-07 20:48:12 +0500

    Added Deleting items and Uodating Cart quantities

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -28,17 +28,18 @@ const cartSlice = createSlice({
     },
     increaseItemQuantity(state, action) {
       // payload pizzaId
-      const item = state.cart.find((item) => item.quantity === action.payload);
+      const item = state.cart.find((item) => item.pizzaId === action.payload);
 
       item.quantity++;
       item.totalPrice = item.quantity * item.unitPrice;
     },
     decreaseItemQuantity(state, action) {
       // payload pizzaId
-      const item = state.cart.find((item) => item.quantity === action.payload);
+      const item = state.cart.find((item) => item.pizzaId === action.payload);
 
       item.quantity--;
       item.totalPrice = item.quantity * item.unitPrice;
+      if (item.quantity === 0) cartSlice.caseReducers.deleteItem(state, action);
     },
     clearCart(state) {
       state.cart = [];
@@ -63,5 +64,8 @@ export const getTotalCartQuantity = (state) =>
 
 export const getTotalCartPrice = (state) =>
   state.cart.cart.reduce((sum, item) => sum + item.totalPrice, 0);
+
+export const getCurrentQuantityById = (id) => (state) =>
+  state.cart.cart.find((item) => item.pizzaId === id)?.quantity ?? 0;
 
 // reselect library
